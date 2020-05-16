b64f8e5832886ef1a71a0987206c7f2df901448a
Slavik Kulinich <slavik-kulinich@seznam.cz>
2020-05-16 11:48:04 +0200

    Deleted antd

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import { Button } from 'antd';
 import './App.css'
 
 const App = () => {
     return(
         <div className="App">
-            <Button type="primary">Button</Button>
+            <button>Click</button>
         </div>
     )
 }
